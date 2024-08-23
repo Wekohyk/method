be945c23450bd202f8cf1cc52f7a0acea81fd865
huangyukai <wekohyk@gmail.com>
2024-08-23 11:05:19 +0800

    refactor: 优化页面布局组件
    对页面布局组件进行了优化，包括删除无用的代码、调整样式和修复 bug。这些改动将提升页面加载性能和用户体验。

--- ios_layout组件/PageLayout.vue
+++ ios_layout组件/PageLayout.vue
@@ -1,50 +1,13 @@
-<script setup lang="ts">
-  import { useAttrs } from 'vue';
-
-  interface Props {
-    transparentNavigationBar?: boolean;
-    hideBack?: boolean;
-    pageTitle?: string;
-    floatHeader?: boolean;
-    scrollable?: boolean;
-    background?: string;
-    navBackgroundColor?: string;
-    enableBackToTop?: boolean;
-  }
-  withDefaults(defineProps<Props>(), { scrollable: true });
-
-  interface Emits {
-    (e: 'back'): void;
-  }
-  const emit = defineEmits<Emits>();
-  interface Slots {
-    default(param: any): void;
-    navigationBarLeading(param: any): any;
-    navigationBarCenter(param: any): any;
-    navigationBarTrailing(param: any): any;
-  }
-  defineSlots<Slots>();
-
-  const attrs = useAttrs();
-  const back = () => {
-    if (Reflect.has(attrs, 'onBack')) {
-      emit('back');
-    } else {
-      tw.router.close();
-    }
-  };
-</script>
-
 <template>
   <div
     class="h-100vh w-full flex flex-col items-stretch bg-top bg-contain bg-no-repeat bg-$primaryBackground"
     :style="{
-      background: background,
+      background,
     }"
   >
     <!-- backdrop-filter backdrop-blur-2 -->
     <header
-      class="top-0 w-full flex-shrink-0 adapt-pt-0 z-1"
+      class="top-0 w-full flex-shrink-0 adapt-pt-0 z-9999999999"
       :class="{
         'bg-transparent': transparentNavigationBar,
         '!fixed left-0': floatHeader,
@@ -54,13 +17,26 @@
       <div class="navigation-bar grid grid-cols-[1fr_auto_1fr]">
         <div class="flex-1 flex items-center h-44 pl-9px">
           <div class="flex items-center mt-1" v-if="!hideBack" @click="back">
-            <GlyIcon
-              FIGMA_KEY="4e671fd0853c20e938ab4920d790da031c4bc3ae"
-              name="nav_icon_back"
-              icon="c81906612980b11a62d17dd6cca40337"
-              color="currentColor"
-              :size="32"
-            />
+            <div class="relative w-26 h-26">
+              <img
+                v-if="isShowGoBackMode"
+                src="/goBackDark.webp"
+                alt=""
+                :style="{
+                  width: goBackWidth,
+                  height: goBackHeight,
+                }"
+              />
+              <img
+                v-else
+                src="/goBackLight.webp"
+                alt=""
+                :style="{
+                  width: goBackWidth,
+                  height: goBackHeight,
+                }"
+              />
+            </div>
           </div>
           <slot name="navigationBarLeading"></slot>
         </div>
@@ -79,9 +55,11 @@
       </div>
     </header>
     <main
+      ref="mainElement"
       v-back-to-top="enableBackToTop"
-      class="relative flex-1 overflow-x-hidden z-999"
+      class="relative flex-1 overflow-x-hidden"
       :style="{ overflowY: scrollable ? 'scroll' : 'hidden' }"
+      @scroll="handleScroll"
     >
       <div
         class="fixed left-0 z-50 adapt-top-44 bottom-0"
@@ -92,3 +70,67 @@
     </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useAttrs, ref } from "vue";
+import { useRouter } from "vue-router";
+
+interface Props {
+  transparentNavigationBar?: boolean;
+  hideBack?: boolean;
+  pageTitle?: string;
+  floatHeader?: boolean;
+  scrollable?: boolean;
+  background?: string;
+  navBackgroundColor?: string;
+  enableBackToTop?: boolean;
+  currentColor?: string;
+  goBackWidth?: string;
+  goBackHeight?: string;
+  isShowGoBackMode?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  scrollable: true,
+  goBackHeight: "18px",
+  goBackWidth: "18px",
+  isShowGoBackMode: true,
+});
+
+interface Emits {
+  (e: "back"): void;
+  (e: "scroll", top: number): void;
+}
+const emit = defineEmits<Emits>();
+
+const mainElement = ref();
+
+const navigatorOpacity = ref(1);
+
+const handleScroll = () => {
+  navigatorOpacity.value = Math.min(1, mainElement.value.scrollTop / 64);
+  emit("scroll", mainElement.value.scrollTop);
+};
+
+interface Slots {
+  default(param: any): void;
+  navigationBarLeading(param: any): any;
+  navigationBarCenter(param: any): any;
+  navigationBarTrailing(param: any): any;
+}
+defineSlots<Slots>();
+
+const router = useRouter();
+const attrs = useAttrs();
+const back = () => {
+  if (Reflect.has(attrs, "onBack")) {
+    emit("back");
+  } else {
+    // tw.router.close();
+    // window.close();
+    // 利用vue-router返回上一页
+    router.back();
+    console.log("关闭当前页面");
+  }
+};
+</script>
